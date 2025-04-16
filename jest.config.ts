533553bd5453573f4f2a4aa47a0e52c433295378
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-04-16 15:18:52 +0530

    chore: initial started template

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
